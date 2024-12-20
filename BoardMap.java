652d05548b5b4552e308285da07656b7505f6890
Denis <denis.y.v@gmail.com>
2024-12-20 09:57:55 +0200

    Refactor `BoardMap` and add utility methods.
    Renamed variables and methods for clarity and consistency. Added `reset` and `printBoard` methods to enhance functionality. Improved code readability by adhering to standard naming conventions.

--- BoardMap.java
+++ BoardMap.java
@@ -1,28 +1,42 @@
 public class BoardMap {
     public int size;
-    private int[][] _map;
+    private int[][] _board;
 
-    public BoardMap(int size)
-    {
+    public BoardMap(int size) {
         this.size = size;
-        _map = new int[size][size];
+        _board = new int[size][size];
     }
 
-    public int Get(int x, int y)
-    {
-        if (onMap(x, y))
-            return _map[size][size];
+    public int get(int x, int y) {
+        if (isValidPosition(x, y))
+            return _board[size][size];
         return -1;
     }
-    public void Set(int x, int y, int number)
-    {
-        if (onMap(x, y))
-            _map[size][size] = number;
+
+    public void set(int x, int y, int number) {
+        if (isValidPosition(x, y))
+            _board[size][size] = number;
     }
 
+    public void reset() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                _board[i][j] = 0;
+            }
+        }
+    }
 
-    public boolean onMap(int x, int y)
-    {
+    public boolean isValidPosition(int x, int y) {
         return x >= 0 && x < size && y >= 0 && y < size;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(_board[i][j] == 0 ? ".    " : String.format("%-5d", _board[i][j]));
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
