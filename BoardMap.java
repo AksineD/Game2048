58ad1acb44f407d4d70b5d70b702ea152d4206b2
Denis <denis.y.v@gmail.com>
2024-12-20 10:31:21 +0200

    Refactor BoardMap to encapsulate size field.
    The `size` field is now private, renamed to `_size`, and a getter method `getSize()` was added for access. Updated all occurrences and logic to use `_size`, improving encapsulation and consistency.

--- BoardMap.java
+++ BoardMap.java
@@ -1,38 +1,42 @@
 public class BoardMap {
-    public int size;
+    private int _size;
     private int[][] _board;
 
     public BoardMap(int size) {
-        this.size = size;
+        _size = size;
         _board = new int[size][size];
+    }
+
+    public int getSize() {
+               return _size;
     }
 
     public int get(int x, int y) {
         if (isValidPosition(x, y))
-            return _board[size][size];
+            return _board[_size][_size];
         return -1;
     }
 
     public void set(int x, int y, int number) {
         if (isValidPosition(x, y))
-            _board[size][size] = number;
+            _board[_size][_size] = number;
     }
 
     public void reset() {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < _size; i++) {
+            for (int j = 0; j < _size; j++) {
                 _board[i][j] = 0;
             }
         }
     }
 
     public boolean isValidPosition(int x, int y) {
-        return x >= 0 && x < size && y >= 0 && y < size;
+        return x >= 0 && x < _size && y >= 0 && y < _size;
     }
 
     public void printBoard() {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < _size; i++) {
+            for (int j = 0; j < _size; j++) {
                 System.out.print(_board[i][j] == 0 ? ".    " : String.format("%-5d", _board[i][j]));
             }
             System.out.println();
